707e3f970111d248711e832fdc06e6b200a89156
Autumn <88445840+autumn-austin@users.noreply.github.com>
2022-08-02 14:10:58 -0400

    added button features & correct/incorrect button feedback

--- QuizMaster/Assets/Scripts/Quiz.cs
+++ QuizMaster/Assets/Scripts/Quiz.cs
@@ -2,15 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class Quiz : MonoBehaviour
 {
-    [SerializeField] TextMeshProUGUI questiontext;
+    [SerializeField] TextMeshProUGUI questionText;
     [SerializeField] QuestionSO question;
     [SerializeField] GameObject[] answerButtons;
+    int correctAnswerIndex;
+    [SerializeField] Sprite defaultAnswerSprite;
+    [SerializeField] Sprite correctAnswerSprite;
     void Start()
     {
-        questiontext.text = question.GetQuestion();
+        questionText.text = question.GetQuestion();
 
         for (int i = 0;i < answerButtons.Length; i++)
         {
@@ -18,6 +22,28 @@ public class Quiz : MonoBehaviour
             buttonText.text = question.GetAnswer(i);
         }
 
+    }
+
+    public void OnAnswerSelected(int index)
+    {
+        Image buttonImage;
+
+        if(index == question.GetCorrectAnswerIndex())
+        {
+            questionText.text = "Correct!";
+            buttonImage = answerButtons[index].GetComponent<Image>();
+            buttonImage.sprite = correctAnswerSprite;
+        }
+        else
+        {
+            correctAnswerIndex = question.GetCorrectAnswerIndex();
+            string correctAnswer = question.GetAnswer(correctAnswerIndex);
+            questionText.text = "Sorry, the correct answerr was;\n" + correctAnswer;
+
+            buttonImage = answerButtons[correctAnswerIndex].GetComponent<Image>();
+            buttonImage.sprite = correctAnswerSprite;
+
+        }
     }
 
 }
